9f31d501cb0a0402c511992f9b84180134198f12
ans-cst <Cloudservices.Servicedesk@ansgroup.co.uk>
2018-02-23 13:58:55 +0000

    Adding Azure AD Scripts

---
+++ Azure/Azure AD/Get-ANS-Users.ps1
@@ -0,0 +1,106 @@
+﻿Write-Host ""
+Write-Host "ANS Azure AD Users"
+Write-Host "Version 1.0.0"
+Write-Host ""
+Write-Host ""
+Write-Host "[$(get-date -Format "dd/mm/yy hh:mm:ss")] Importing module..."
+
+#Install and Import AzureAD Module
+Write-Host "[$(get-date -Format "dd/mm/yy hh:mm:ss")] Importing module..."
+Import-Module -Name AzureAD -ErrorVariable ModuleError -ErrorAction SilentlyContinue
+If ($ModuleError) {
+    Write-Host "[$(get-date -Format "dd/mm/yy hh:mm:ss")] Installing module..."
+    Install-Module -Name AzureAD
+    Import-Module -Name AzureAD
+    Write-Host "[$(get-date -Format "dd/mm/yy hh:mm:ss")] Successfully Installed module..."
+}
+Write-Host "[$(get-date -Format "dd/mm/yy hh:mm:ss")] Successfully Imported module"
+Write-Host ""
+
+#Get the credentials from the Credentials txt file
+$FilePath = Read-Host "Please input the directory path to credentials file"
+$credentailsAndGroupNames = Get-Content "$FilePath\Credentials & Group Names.txt"
+$ansUsername = $($credentailsAndGroupNames[2] -split ": ")[1]
+$ansPassword = $($credentailsAndGroupNames[3] -split ": ")[1]
+$ansPassword = ConvertTo-SecureString $ansPassword -AsPlainText -Force
+
+$secondLineGroupName = $($credentailsAndGroupNames[8] -split ": ")[1]
+$thirdLineGroupName = $($credentailsAndGroupNames[9] -split ": ")[1]
+
+#Loging with the ANS Azure AD Admin account
+Write-Host "[$(get-date -Format "dd/mm/yy hh:mm:ss")] Connecting to the ANS Azure AD Admin account..."
+
+$credentials = New-Object System.Management.Automation.PSCredential ($ansUsername, $ansPassword)
+Connect-AzureAD -Credential $credentials
+
+Write-Host "[$(get-date -Format "dd/mm/yy hh:mm:ss")] Login successful"
+Write-Host ""
+
+
+#Create the 2nd Line and 3rd Line placeholders
+$secondLineMembersCsvString = $null
+$thirdLineMembersCsvString = $null
+
+$secondLineMembersCsvString = @"
+DisplayName,Email
+
+"@
+
+
+$thirdLineMembersCsvString = @"
+DisplayName,Email
+
+"@
+
+
+#Get Group Memberships
+
+#Get 2ndLine Group Members
+Write-Host "[$(get-date -Format "dd/mm/yy hh:mm:ss")] Getting 2nd Line Group Membership..."
+
+$2ndLineGroup = Get-AzureADGroup -SearchString $secondLineGroupName
+$2ndLineMembers = Get-AzureADGroupMember -ObjectId $2ndLineGroup.ObjectId
+
+foreach ($Member in $2ndLineMembers) {
+    #Write Users UPN and Display Name to CSV
+    $email = $Member.UserPrincipalName
+    $secondLineMembersCsvString += @" 
+$($member.DisplayName),$email
+
+"@
+    }
+Write-Host "[$(get-date -Format "dd/mm/yy hh:mm:ss")] 2nd Line Group Membership Complete"
+
+
+
+Write-Host "[$(get-date -Format "dd/mm/yy hh:mm:ss")] Getting 3rd Line Group Membership..."
+
+#Get 3rdLine Group Members
+$3rdLineGroup = Get-AzureADGroup -SearchString $thirdLineGroupName
+$3rdLineMembers = Get-AzureADGroupMember -ObjectId $3rdLineGroup.ObjectId
+
+foreach ($Member in $3rdLineMembers) {
+    #Write Users UPN and Display Name to CSV
+    $email = $Member.UserPrincipalName
+    $thirdLineMembersCsvString += @" 
+$($member.DisplayName),$email
+
+"@
+    }
+Write-Host "[$(get-date -Format "dd/mm/yy hh:mm:ss")] 3rd Line Group Membership Complete"
+
+#Output the 2nd Line users to a CSV file
+Write-Host "[$(get-date -Format "dd/mm/yy hh:mm:ss")] Writing out CSV file $FilePath\2nd Line ANS Users.csv"
+Out-File -InputObject $secondLineMembersCsvString -FilePath "$FilePath\2nd Line ANS Users.csv"
+
+#Output the 3rd Line users to a CSV file
+Write-Host "[$(get-date -Format "dd/mm/yy hh:mm:ss")] Writing out CSV file $FilePath\3rd Line ANS Users.csv"
+Out-File -InputObject $thirdLineMembersCsvString -FilePath "$FilePath\3rd Line ANS Users.csv"
+
+#Disconnect from the ANS Azure AD
+Write-Host ""
+Write-Host "[$(get-date -Format "dd/mm/yy hh:mm:ss")] Disconnecting from ANS Azure AD..."
+Disconnect-AzureAD
+Write-Host "[$(get-date -Format "dd/mm/yy hh:mm:ss")] Successfully disconnected"
+Write-Host ""
+Write-Host ""
---
+++ Azure/Azure AD/Import-ANS-Users-AzureAD.ps1
@@ -0,0 +1,157 @@
+﻿Write-Host ""
+Write-Host "ANS Azure AD Users to Customer Directory"
+Write-Host "Version 1.0.0"
+Write-Host ""
+Write-Host ""
+
+#Install and Import AzureAD Module
+Write-Host "[$(get-date -Format "dd/mm/yy hh:mm:ss")] Importing module..."
+Import-Module -Name AzureAD -ErrorVariable ModuleError -ErrorAction SilentlyContinue
+If ($ModuleError) {
+    Write-Host "[$(get-date -Format "dd/mm/yy hh:mm:ss")] Installing module..."
+    Install-Module -Name AzureAD
+    Import-Module -Name AzureAD
+    Write-Host "[$(get-date -Format "dd/mm/yy hh:mm:ss")] Successfully Installed module..."
+}
+Write-Host "[$(get-date -Format "dd/mm/yy hh:mm:ss")] Successfully Imported module"
+Write-Host ""
+
+#Login to Azure AD
+Write-Host "[$(get-date -Format "dd/mm/yy hh:mm:ss")] Connecting to Azure AD..."
+$cred = Get-Credential
+Connect-AzureAD -Credential $cred
+Write-Host "[$(get-date -Format "dd/mm/yy hh:mm:ss")] Connected to Azure AD successfully"
+Write-Host ""
+
+
+#Set CSV Directory Path and Import CSVs
+$CSVPath = Read-Host "Please input the directory path to the CSV locations"
+$secondLineInvitations = import-csv "$CSVPath\2nd Line ANS Users.csv"
+$thirdLineInvitation = import-csv "$CSVPath\3rd Line ANS Users.csv"
+$invitations = $secondLineInvitations + $thirdLineInvitation
+Write-Host "[$(get-date -Format "dd/mm/yy hh:mm:ss")] Imported CSV successfully!"
+Write-Host ""
+
+
+#Create Invitations
+$messageInfo = New-Object Microsoft.Open.MSGraph.Model.InvitedUserMessageInfo
+$messageInfo.customizedMessageBody = "Hey there! Check this out. I created an invitation through PowerShell"
+
+
+foreach ($invite in $invitations) {
+    #Check if the user exists within the directory
+    $result = Get-AzureADUser -SearchString $invite.DisplayName
+
+    #Check if the user doesnt exist before sending the invitation
+    if ($result -eq $null) {
+        Write-Host "Sending Invitation to " $invite.DisplayName
+        New-AzureADMSInvitation -InvitedUserEmailAddress $invite.Email -InvitedUserDisplayName $invite.DisplayName -InviteRedirectUrl https://portal.azure.com -InvitedUserMessageInfo $messageInfo -SendInvitationMessage $true
+    }
+    else {
+    Write-Host "[$(get-date -Format "dd/mm/yy hh:mm:ss")] User $($invite.DisplayName) already exists within the customers directory"
+    }
+}
+
+Write-Host "[$(get-date -Format "dd/mm/yy hh:mm:ss")] Invitiations sent successfully."
+Write-Host ""
+
+
+
+#Create AzureAD groups
+Write-Host "[$(get-date -Format "dd/mm/yy hh:mm:ss")] Creating 2nd and 3rd Line Azure AD Groups..."
+
+#Check if the 2ndLine Group exists
+$2ndLineGroupName = "ANS-SecondLine"
+$result = Get-AzureADGroup -SearchString $2ndLineGroupName
+$2ndLineGroup = $null
+
+if ($result -eq $null) {
+    #If the group does not exist create the group
+    $2ndLineGroup = New-AzureADGroup -DisplayName $2ndLineGroupName -MailEnabled $false -MailNickName "ANS-2nd-Line" -SecurityEnabled $true -Description "ANS $2ndLineGroupName security group"
+
+}
+else {
+    Write-Host "[$(get-date -Format "dd/mm/yy hh:mm:ss")] 2nd line group already exists"
+    #If the Group exists then get the Group
+    $2ndLineGroup = Get-AzureADGroup -SearchString $2ndLineGroupName
+}
+
+#Check if the 3rdLine Group exists
+$3rdLineGroupName = "ANS-ThirdLine"
+$result = Get-AzureADGroup -SearchString $3rdLineGroupName
+$3rdLineGroup = $null
+
+if ($result -eq $null) {
+    #If the group does not exist create the group
+    $3rdLineGroup = New-AzureADGroup -DisplayName $3rdLineGroupName -MailEnabled $false -MailNickName "ANS-2nd-Line" -SecurityEnabled $true -Description "ANS $3rdLineGroupName security group"
+
+}
+else {
+    Write-Host "[$(get-date -Format "dd/mm/yy hh:mm:ss")] 3rd line group already exists"
+    #If the Group exists then get the Group
+    $3rdLineGroup = Get-AzureADGroup -SearchString $3rdLineGroupName
+}
+Write-Host "[$(get-date -Format "dd/mm/yy hh:mm:ss")] Azure AD Groups created successfully"
+Write-Host ""
+
+
+
+#Create the users within the 2nd Line Group
+Write-Host "[$(get-date -Format "dd/mm/yy hh:mm:ss")] Adding ANS users to the new 2nd Line group..."
+foreach ($invite1 in $secondLineInvitations) {
+    $User1 = Get-AzureADUser -SearchString $invite1.DisplayName
+    while (!$User1) {
+        Write-Host "Waiting for "$invite1.DisplayName
+        SLEEP 1
+        $User1 = Get-AzureADUser -SearchString $invite1.DisplayName
+    }
+
+        #Get User Membership
+	    $Membership = Get-AzureADUserMembership  -ObjectId $User1.ObjectId
+
+            #If user exists do nothing
+            If ($Membership.ObjectId -contains $2ndLineGroup.ObjectId) {
+	            Write-Host "[$(get-date -Format "dd/mm/yy hh:mm:ss")] User" $User1.DisplayName "already exists within the customers directory"
+	        }
+            #If the user does not exist add the user to the group
+    	    else {
+                Add-AzureADGroupMember -ObjectId $2ndLineGroup.ObjectId -RefObjectId $User1.ObjectId
+    	    }
+}
+
+Write-Host "[$(get-date -Format "dd/mm/yy hh:mm:ss")] Added ANS users to 2nd Line group"
+Write-Host ""
+
+#Create the users within the 3rd Line Group
+Write-Host "[$(get-date -Format "dd/mm/yy hh:mm:ss")] Adding ANS users to the new 3rd Line group..."
+foreach ($invite2 in $thirdLineInvitation) {
+    $User2 = Get-AzureADUser -SearchString $invite2.DisplayName
+    while (!$User2) {
+        Write-Host "[$(get-date -Format "dd/mm/yy hh:mm:ss")] Waiting for "$invite2.DisplayName
+        SLEEP 1
+        $User2 = Get-AzureADUser -SearchString $invite2.DisplayName
+    }
+
+        #Get User Membership
+	    $Membership = (Get-AzureADUserMembership  -ObjectId $User2.ObjectId | Select ObjectId)
+
+            #If user exists do nothing
+            If ($Membership.ObjectId -contains $3rdLineGroup.ObjectId) {
+	            Write-Host "[$(get-date -Format "dd/mm/yy hh:mm:ss")] User" $User2.DisplayName "already exists within the customers directory"
+	        }
+
+            #If the user does not exist add the user to the group
+	        else {
+                Add-AzureADGroupMember -ObjectId $3rdLineGroup.ObjectId -RefObjectId $User2.ObjectId
+            }
+}
+Write-Host "[$(get-date -Format "dd/mm/yy hh:mm:ss")] Added ANS users to 3rd Line group"
+
+
+#Disconnect from the Customers Azure AD
+Write-Host ""
+Write-Host "[$(get-date -Format "dd/mm/yy hh:mm:ss")] Disconnecting from the customers Azure AD..."
+Disconnect-AzureAD
+Write-Host "[$(get-date -Format "dd/mm/yy hh:mm:ss")] Successfully disconnected"
+Write-Host ""
+Write-Host "[$(get-date -Format "dd/mm/yy hh:mm:ss")] Done!"
