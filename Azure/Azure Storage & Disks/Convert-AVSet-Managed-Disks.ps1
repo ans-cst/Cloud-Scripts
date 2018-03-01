067ac40ee0d54e02f4c7d744672c1e057c283f35
ans-cst <Cloudservices.Servicedesk@ansgroup.co.uk>
2018-03-01 16:11:55 +0000

    Updated Convert to Managed Disk Scripts to include host output and wait time

--- Azure/Azure Storage & Disks/Convert-AVSet-Managed-Disks.ps1
+++ Azure/Azure Storage & Disks/Convert-AVSet-Managed-Disks.ps1
@@ -1,23 +1,61 @@
+Write-Host ""
+Write-Host "ANS Convert Azure Availabiltiy Set to Managed Disks"
+Write-Host "Version 1.0.0"
+Write-Host ""
+Write-Host ""
+
+#Install and Import AzureRM Module
+Write-Host "[$(get-date -Format "dd/mm/yy hh:mm:ss")] Importing module..."
+Import-Module -Name AzureRM -ErrorVariable ModuleError -ErrorAction SilentlyContinue
+If ($ModuleError) {
+    Write-Host "[$(get-date -Format "dd/mm/yy hh:mm:ss")] Installing module..."
+    Install-Module -Name AzureRM
+    Import-Module -Name AzureRM
+    Write-Host "[$(get-date -Format "dd/mm/yy hh:mm:ss")] Successfully Installed module..."
+}
+Write-Host "[$(get-date -Format "dd/mm/yy hh:mm:ss")] Successfully Imported module"
+Write-Host ""
+
 #Set Environment Variables
-$rgName = ""
-$avSetName = ""
-$subscriptionId = ""
+$vmName = Read-Host "Please input the Virtual Machines Name"
+$rgName = Read-Host "Please input the Virtual Machines Resource Group Name"
+[uint16]$Time = Read-Host "Please input the amount of time to wait between converting each VM in seconds"
 
 #Login to Azure Environment
+Write-Host "[$(get-date -Format "dd/mm/yy hh:mm:ss")] Logging in to Azure Account..."
 Login-AzureRmAccount
+Write-Host "[$(get-date -Format "dd/mm/yy hh:mm:ss")] Successfully logged in to Azure Account"
+Write-Host ""
 
 #Set Subscription Id
-Select-AzureRmSubscription -SubscriptionId 
+$SubId = Read-Host "Please input your Subscription Id"
+while ($SubId.Length -le 35) {
+    Write-Host "[$(get-date -Format "dd/mm/yy hh:mm:ss")] Subscription Id not valid"
+    $SubId = Read-Host "Please input your Subscription Id"
+}
+Select-AzureRmSubscription -SubscriptionId $SubId
+Write-Host "[$(get-date -Format "dd/mm/yy hh:mm:ss")] Subscription successfully selected"
+Write-Host ""
 
 #Convert Availability set to Managed
+Write-Host "[$(get-date -Format "dd/mm/yy hh:mm:ss")] Changing Availability set SKU to aligned"
 $avSet = Get-AzureRmAvailabilitySet -ResourceGroupName $rgName -Name $avSetName
 Update-AzureRmAvailabilitySet -AvailabilitySet $avSet -Sku Aligned 
+Write-Host "[$(get-date -Format "dd/mm/yy hh:mm:ss")] Availability set updated successfully"
 
 #Convert VM disks to managed
 $avSet = Get-AzureRmAvailabilitySet -ResourceGroupName $rgName -Name $avSetName
-foreach($vmInfo in $avSet.VirtualMachinesReferences)
+foreach($vm in $avSet.VirtualMachinesReferences)
 {
   $vm = Get-AzureRmVM -ResourceGroupName $rgName | Where-Object {$_.Id -eq $vmInfo.id}
+
+  Write-Host "[$(get-date -Format "dd/mm/yy hh:mm:ss")] Stopping Virtual Machine -" $vm.Name
+
   Stop-AzureRmVM -ResourceGroupName $rgName -Name $vm.Name -Force
   ConvertTo-AzureRmVMManagedDisk -ResourceGroupName $rgName -VMName $vm.Name
+  Start-AzureRmVM -ResourceGroupName $rgName -Name $vm.Name
+
+  Write-Host "[$(get-date -Format "dd/mm/yy hh:mm:ss")] Virtual Machine" $vm.Name "started successfully"
+  Write-Host "Waiting for previous VM to boot and start application services - waiting for" $Time "seconds"
+  SLEEP $TIME
 }
--- Azure/Azure Storage & Disks/Convert-VM-Managed-Disks.ps1
+++ Azure/Azure Storage & Disks/Convert-VM-Managed-Disks.ps1
@@ -1,14 +1,54 @@
+Write-Host ""
+Write-Host "ANS Convert Azure VM to Managed Disks"
+Write-Host "Version 1.0.0"
+Write-Host ""
+Write-Host ""
+
+#Install and Import AzureRM Module
+Write-Host "[$(get-date -Format "dd/mm/yy hh:mm:ss")] Importing module..."
+Import-Module -Name AzureRM -ErrorVariable ModuleError -ErrorAction SilentlyContinue
+If ($ModuleError) {
+    Write-Host "[$(get-date -Format "dd/mm/yy hh:mm:ss")] Installing module..."
+    Install-Module -Name AzureRM
+    Import-Module -Name AzureRM
+    Write-Host "[$(get-date -Format "dd/mm/yy hh:mm:ss")] Successfully Installed module..."
+}
+Write-Host "[$(get-date -Format "dd/mm/yy hh:mm:ss")] Successfully Imported module"
+Write-Host ""
+
 #Set Environment Variables
-$rgName = "POC"
-$vmName = "Test-Linux"
-$SubscriptionId = ""
+$vmName = Read-Host "Please Input the Virtual Machines Name"
+$rgName = Read-Host "Please Input the Virtual Machines Resource Group Name"
+
 
 #Login to Azure Environment
+Write-Host "[$(get-date -Format "dd/mm/yy hh:mm:ss")] Logging in to Azure Account..."
 Login-AzureRmAccount
+Write-Host "[$(get-date -Format "dd/mm/yy hh:mm:ss")] Successfully logged in to Azure Account"
+Write-Host ""
 
 #Set Subscription Id
-Select-AzureRmSubscription -SubscriptionId $SubscriptionId
+$SubId = Read-Host "Please input your Subscription Id"
+while ($SubId.Length -le 35) {
+    Write-Host "[$(get-date -Format "dd/mm/yy hh:mm:ss")] Subscription Id not valid"
+    $SubId = Read-Host "Please input your Subscription Id"
+}
+Select-AzureRmSubscription -SubscriptionId $SubId
+Write-Host "[$(get-date -Format "dd/mm/yy hh:mm:ss")] Subscription successfully selected"
+Write-Host ""
+
+#Stop Virtual Machine
+Write-Host "[$(get-date -Format "dd/mm/yy hh:mm:ss")] Stopping Virtual Machine" $vmName
+Stop-AzureRmVM -ResourceGroupName $rgName -Name $vmName -Force
+
+$VMState = Get-AzureRmVm -ResourceGroupName $rgName -Name $vmName -Status
+while ($VMState.Statuses.DisplayStatus[1] -ne "VM deallocated") {
+    Write-Host "Check the status of" $vmName
+    SLEEP 2
+}
+Write-Host "[$(get-date -Format "dd/mm/yy hh:mm:ss")]" $vmName "has successfully stopped"
 
 #Convert VM to Managed Disks
-Stop-AzureRmVM -ResourceGroupName $rgName -Name $vmName -Force
+Write-Host "[$(get-date -Format "dd/mm/yy hh:mm:ss")] Converting Virtual Machine" $vmName "to managed disks"
 ConvertTo-AzureRmVMManagedDisk -ResourceGroupName $rgName -VMName $vmName
+Write-Host "[$(get-date -Format "dd/mm/yy hh:mm:ss")] Converted Virtual Machine" $vmName "to managed disks successfully!"
