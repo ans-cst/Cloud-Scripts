ea4f4a1f2913f099efc8664b6c29e58e51a41a4f
ans-cst <Cloudservices.Servicedesk@ansgroup.co.uk>
2018-03-06 15:23:35 +0000

    Fixed a number of issues in the script

--- AWS/PV to HVM Migration.ps1
+++ AWS/PV to HVM Migration.ps1
@@ -1,97 +1,230 @@
-﻿#Script for Migrating to HVM Based AMI
+﻿Write-Host ""
+Write-Host "ANS ParaVirtual to Hardware Virtual Machine Kernel Migration"
+Write-Host "Version 2.0.0"
+Write-Host ""
+Write-Host ""
 
-#Set Environment Variables
-$Region = ""
-$AZ = ""
-$Account = ""
-$AccessKey = ""
-$SecretKey = ""
-$Parameter = "@{'commands'=@('Command1', 'Command2')}"
-$PVInstanceID = ""
-$HVMami = ""
-$InstanceType = ""
-$TempDeviceID = ""
-$RootDeviceID = ""
+#Install and Import AWS PowerShell Module
+Write-Host "[$(get-date -Format "dd/mm/yy hh:mm:ss")] Importing module..."
+Import-Module -Name AWSPowerShell -ErrorVariable ModuleError -ErrorAction SilentlyContinue
+If ($ModuleError) {
+    Write-Host "[$(get-date -Format "dd/mm/yy hh:mm:ss")] Installing module..."
+    Install-Module -Name AWSPowerShell
+    Import-Module -Name AWSPowerShell
+    Write-Host "[$(get-date -Format "dd/mm/yy hh:mm:ss")] Successfully Installed module..."
+}
+Write-Host "[$(get-date -Format "dd/mm/yy hh:mm:ss")] Successfully Imported module"
+Write-Host ""
 
-#Logging in to Account
-Write-Host "Logging in to " $Account " Account"
+#Login to AWS Account
+$AccessKey = Read-Host "Please input your AWS Access Key"
+$SecretKey = Read-Host "Please input your AWS Secret Key"
+Write-Host ""
+Write-Host "[$(get-date -Format "dd/mm/yy hh:mm:ss")] Logging in to AWS Account..."
 Set-AWSCredentials -AccessKey $AccessKey -SecretKey $SecretKey ;
+Write-Host "[$(get-date -Format "dd/mm/yy hh:mm:ss")] Successfully logged in to AWS Account"
+Write-Host ""
 
-#Get Attached EBS Volume IDs
-$PVVolume = (Get-EC2Volume -Region $Region | ? { $_.Attachments.InstanceId -eq $PVInstanceId}).VolumeId
-$VolumeType = (Get-EC2Volume -Region $Region| ? { $_.Attachments.InstanceId -eq $PVInstanceId}).VolumeType
+#Get PV Instance Root Volume
+$Region = Read-Host "Please input your AWS Region"
+$PVInstanceID = Read-Host "Please input your Instance ID for your ParaVirtual Instance"
+$PVVolumes = (Get-EC2Volume -Region $Region | ? { $_.Attachments.InstanceId -eq $PVInstanceId})
+$PVVolume = (Get-EC2Volume -Region $Region | ? { $_.Attachments.InstanceId -eq $PVInstanceId})[0]
 
-#Create Snapshot of EBS Volume
-Write-Host "Creating EBS Volume Snapshot of PV Volume"
-$SnapshotId = New-EC2Snapshot -Region $Region -VolumeId $VolumeID -Description "Migrating to HVM Based AMI"
-Write-Host "EBS Snapshot Created Successfully"
+#Create Snapshot of PV Instance Root Volume
+Write-Host ""
+Write-Host "[$(get-date -Format "dd/mm/yy hh:mm:ss")] Creating EBS Volume Snapshot of ParaVirtual Volume"
+$Snapshot = New-EC2Snapshot -Region $Region -VolumeId $PVVolume.VolumeId -Description "Migrating to HVM Based AMI"
+Write-Host "[$(get-date -Format "dd/mm/yy hh:mm:ss")] EBS Snapshot Created Successfully"
+Write-Host ""
 
-#Confirm the Instance ID of your new HVM Instance
-Write-Host "Creating new HVM Based EC2 Instance"
-$HVMInstanceId = (New-EC2Instance -ImageId $HVMAMI -AvailabilityZone $AZ -Region $Region -InstanceType $InstanceType -MaxCount 1).Instances
-$HVMInstanceId = $HVMInstanceId.InstanceId
 
-#Create EBS Volume from Snapshot
-Write-Host "Creating EBS Volume from PV Snapshot"
-$PVVolumeId = New-EC2Volume -Region $Region -AvailabilityZone $AZ -SnapshotId $SnapshotId.SnapshotId -VolumeType $VolumeType
-Write-Host "EBS Volume Created Successfully"
+#Input the Instance details of your new HVM Instance
+$HVMName = Read-Host "Please input the name for your new HVM Instance"
+$HVMAMI = Read-Host "Please input the AMI ID for your new HVM Instance"
+$HVMType = Read-Host "Please input the instance type for your new HVM Instance"
+Write-Host ""
+Write-Host "[$(get-date -Format "dd/mm/yy hh:mm:ss")] Creating new HVM Based EC2 Instance"
+
+#Get PV Instance Configuration
+$PVInstance =  (Get-EC2Instance -Region $Region -InstanceId $PVInstanceID).Instances
+
+#Build HVM Instance Tags
+$Tags = new-object Amazon.EC2.Model.TagSpecification
+$Tags.ResourceType = "instance"
+
+#Copy Tags from PV Instance
+foreach ($Key in $PVInstance.Tags | Where-Object {$_.Key -ne "Name"}) {
+    $Tag = @{ key = $Key.key; Value = $Key.Value}
+    $Tags.Tags.Add($Tag)
+}
+#Set New Name Tag
+$Tag = @{ key = "Name"; Value = $HVMName} 
+$Tags.Tags.Add($Tag)
+
+#Create the HVM Instance
+$HVMInstance = (New-EC2Instance -ImageId $HVMAMI -AvailabilityZone $PVVolume.AvailabilityZone -Region $Region -InstanceType $HVMType -MaxCount 1 `
+-KeyName $PVInstance.KeyName -SecurityGroupId $PVInstance.SecurityGroups.GroupId -SubnetId $PVInstance.SubnetId -InstanceProfile_Name $PVInstance.IamInstanceProfile `
+-TagSpecification $Tags).Instances
+
+Write-Host "[$(get-date -Format "dd/mm/yy hh:mm:ss")] New HVM Instance" $HVMInstance.InstanceId "created successfully"
+Write-Host ""
+
+#Wait for Snapshot to enter the completed state
+Do {
+$SnapshotState = (Get-EC2Snapshot -Region $region -SnapshotId $Snapshot.SnapshotId).State
+Write-Host "[$(get-date -Format "dd/mm/yy hh:mm:ss")] Waiting for snapshot to enter the completed state"
+Write-Host ""
+SLEEP 5
+}
+Until ($SnapshotState = "completed")
+
+#Create New EBS Volume from PV Root Volume Snapshot
+Write-Host "[$(get-date -Format "dd/mm/yy hh:mm:ss")] Creating EBS Volume from ParaVirtual Snapshot"
+$MigrationVolume = New-EC2Volume -Region $Region -AvailabilityZone $PVVolume.AvailabilityZone -SnapshotId $Snapshot.SnapshotId -VolumeType $PVVolume.VolumeType.Value
+Write-Host "[$(get-date -Format "dd/mm/yy hh:mm:ss")] Created EBS Volume from ParaVirtual Snapshot successfully"
+Write-Host ""
+
+#Wait for HVM Instance to enter the running state
+Do {
+    $HVMInstanceState = (Get-EC2Instance -InstanceId $HVMInstance.InstanceId -Region $Region).Instances.State.Name.Value
+    Write-Host "[$(get-date -Format "dd/mm/yy hh:mm:ss")] Waiting for instance to enter the running state"
+    SLEEP 5
+}
+Until ($HVMInstanceState -eq "running")
+Write-Host "[$(get-date -Format "dd/mm/yy hh:mm:ss")] Instance" $HVMInstance.InstanceId "has now entered the running state"
+Write-Host ""
+
 
 #Check New EBS Volume is Available
-$PVVolumeId = Get-EC2Volume -Region $Region -VolumeId $PVVolumeId.VolumeId
-Do {($VolumeState = (Get-EC2Volume -Region $Region -VolumeId $PVVolumeId.VolumeId).State)}
-Until ($VolumeState -eq "available")
+Do {
+    $MigrationVolumeState = (Get-EC2Volume -Region $Region -VolumeId $MigrationVolume.VolumeId).State
+    Write-Host "[$(get-date -Format "dd/mm/yy hh:mm:ss")] Waiting for Volume to finish creating"
+    SLEEP 5
+}
+Until ($MigrationVolumeState -eq "available")
+Write-Host "[$(get-date -Format "dd/mm/yy hh:mm:ss")] EBS Volume" $MigrationVolume.VolumeId "Created Successfully"
+Write-Host ""
 
-#Attach New EBS volume to new Instance ID
-Write-Host "Attaching EBS Volume to HVM Instance"
-Add-EC2Volume -Region $Region -InstanceId $HVMInstanceId -VolumeId $PVVolumeId.VolumeId -Device $TempDeviceID
-Write-Host "EBS Volume Attached to HVM Instance successfully"
+#Attach the New EBS volume to the new HVM Instance
+Write-Host "[$(get-date -Format "dd/mm/yy hh:mm:ss")] Attaching EBS Volume to HVM Instance"
+Add-EC2Volume -Region $Region -InstanceId $HVMInstance.InstanceId -VolumeId $MigrationVolume.VolumeId -Device /dev/sdm
+Write-Host "[$(get-date -Format "dd/mm/yy hh:mm:ss")] EBS Volume Attached to HVM Instance successfully"
+Write-Host ""
 
-#Run Command to Migrate HVM Kernel to EBS Volume
-$Command = Send-SSMCommand -DocumentName AWS-RunShellScript -InstanceId $HVMInstanceId -Parameter $Parameter
+
+
+#Run Commands to Migrate HVM Kernel to EBS Volume
+
+#$OSHashTable = @{ AmazonLinux = "URL" ; RedHat = "URL" ; CentOS = "" ; Windows = "URL" } ;
+#Write-Host "Please enter one of the below Operating System Versions: 
+#AmazonLinux 
+#RedHat 
+#CentOS 
+#Windows"
+#
+#$OSVersion = Read-Host "Please input your OS Version for the kernal migration"
+#if ($OSHashTable -notcontains $OSVersion) {
+#    $OSVersion = Read-Host "Please input your OS Version for the kernal migration"
+#}
+
+#$Parameter = "@{'commands'=@('Command1', 'Command2')}"
+#$Command = Send-SSMCommand -DocumentName AWS-RunShellScript -InstanceId $HVMInstanceId -Parameter $Parameter
 
 #Check SSM Command Status
-Do  {($CommandState = (Get-SSMCommand -CommandId $Command.CommandId)}
-Until ($CommandState -eq "Success")
-Get-SSMCommand -CommandId $Command.CommandId
+#Do  {
+#$CommandState = (Get-SSMCommand -CommandId $Command).CommandId
+#}
+#Until ($CommandState -eq "Success")
 
-#Prompt User to continue once kernel has been copied
-Write-Host "Please ensure tasks to copy HVM Kernel to your PV EBS Volume have completed"
+
+
+#Prompt User to continue once kernel has been copied successfully
+Write-Host ""
+Write-Host "[$(get-date -Format "dd/mm/yy hh:mm:ss")] Please ensure tasks to copy HVM Kernel to your PV EBS Volume have completed"
 Do {
-    $UserInput = Read-Host -Prompt "Type Continue Once Completed"
+    $UserInput = Read-Host -Prompt "Please type continue once kernal migration is completed"
     if ($UserInput -eq "Continue")
-        {"Continuing Script"}
+        {"[$(get-date -Format "dd/mm/yy hh:mm:ss")] Continuing Script"}
     else
         {Read-Host "Type Continue once completed"}
    }
 Until ($UserInput -eq "Continue")
 
-#Stop Instance and report back once in a stopped state
-Stop-EC2Instance -Region $Region -InstanceId $HVMInstanceId
-Write-Host "Allowing EC2 Instance to stop"
-Do  {($InstanceState = (Get-EC2Instance -Region $Region -InstanceId $HVMInstanceId).Instances.State.Name.Value)}
+#Stop HVM Instance and report back once in a stopped state
+Write-Host ""
+Write-Host "[$(get-date -Format "dd/mm/yy hh:mm:ss")] Stopping EC2 Instance" $HVMInstance.InstanceId 
+Stop-EC2Instance -Region $Region -InstanceId $HVMInstance.InstanceId
+
+Do  {
+($InstanceState = (Get-EC2Instance -Region $Region -InstanceId $HVMInstance.InstanceId).Instances.State.Name.Value)
+(SLEEP 5)    
+}
 Until ($InstanceState -eq "stopped")
-Write-Host "EC2 Instance is now stopped"
+Write-Host "[$(get-date -Format "dd/mm/yy hh:mm:ss")] EC2 Instance is now stopped"
 
-#Detach EBS Volumes from HVM Instance
-Write-Host "Detaching EBS Volumes from HVM Instance"
-$HVMVolumeID = (Get-EC2Volume -Region $Region | ? { $_.Attachments.InstanceId -eq $HVMInstanceId}).VolumeId
-Dismount-EC2Volume -Region $Region -InstanceId $HVMInstanceId -VolumeId $HVMVolumeID[0]
-Dismount-EC2Volume -Region $Region -InstanceId $HVMInstanceId -VolumeId $HVMVolumeID[1]
-Write-Host "EBS Volumes detached successfully"
+#Detach All EBS Volumes from HVM Instance
+Write-Host ""
+Write-Host "[$(get-date -Format "dd/mm/yy hh:mm:ss")] Detaching EBS Volumes from HVM Instance" $HVMInstance.InstanceId
+$HVMVolume = (Get-EC2Volume -Region $Region | ? { $_.Attachments.InstanceId -eq $HVMInstance.InstanceId})
+foreach ($Volume in $HVMVolume) {
+    Dismount-EC2Volume -Region $Region -InstanceId $HVMInstance.InstanceId -VolumeId $Volume.VolumeId
+    Do {
+    ($VolumeState = (Get-EC2Volume -Region $Region -VolumeId $Volume.VolumeId).State)
+    (SLEEP 2)
+    }
+    Until ($VolumeState -eq "available")
+}
+Write-Host "[$(get-date -Format "dd/mm/yy hh:mm:ss")] EBS Volumes detached successfully"
 
-#Checking EBS Volumes are available
-Write-Host "Waiting for EBS Volumes to detach"
-Do {($VolumeState = (Get-EC2Volume -Region $Region -VolumeId $HVMVolumeID[0]).State)}
-Until ($VolumeState -eq "available")
-Do {($VolumeState = (Get-EC2Volume -Region $Region -VolumeId $HVMVolumeID[1]).State)}
-Until ($VolumeState -eq "available")
-
-#Re-attach PV EBS Volume as root device
-Write-Host "Attaching EBS Volume to HVM Instance as Root Device"
-Add-EC2Volume -Region $Region -InstanceId $HVMInstanceId -VolumeId $PVVolumeId.VolumeId -Device $RootDeviceID
-Write-Host "EBS Volume attached as root device successfully"
+#Re-attach New Root Volume as root device to HVM Instance
+Write-Host ""
+Write-Host "[$(get-date -Format "dd/mm/yy hh:mm:ss")] Attaching EBS Volume to HVM Instance as Root Device"
+Add-EC2Volume -Region $Region -InstanceId $HVMInstance.InstanceId -VolumeId $MigrationVolume.VolumeId -Device /dev/xvda
+Write-Host "[$(get-date -Format "dd/mm/yy hh:mm:ss")] EBS Volume attached as root device successfully"
 
 #Start HVM Instance
-Write-Host "Starting New HVM Instance"
-Start-EC2Instance -Region $Region -InstanceId $HVMInstanceId
-Write-Host "HVM Instance started successfully"
+Write-Host ""
+Write-Host "[$(get-date -Format "dd/mm/yy hh:mm:ss")] Starting New HVM Instance" $HVMInstance.InstanceId
+Start-EC2Instance -Region $Region -InstanceId $HVMInstance.InstanceId
+Do {
+    $HVMInstanceState = (Get-EC2Instance -InstanceId $HVMInstance.InstanceId -Region $Region).Instances.State.Name.Value
+    Write-Host "[$(get-date -Format "dd/mm/yy hh:mm:ss")] Waiting for instance to enter the running state"
+    SLEEP 5
+}
+Until ($HVMInstanceState -eq "running")
+Write-Host "[$(get-date -Format "dd/mm/yy hh:mm:ss")] HVM Instance" $HVMInstance.InstanceId "started successfully"
+
+
+#Stop PV Instance
+Write-Host ""
+Write-Host "[$(get-date -Format "dd/mm/yy hh:mm:ss")] Stopping EC2 Instance" $PVInstance.InstanceId 
+Stop-EC2Instance -Region $Region -InstanceId $PVInstance.InstanceId
+
+Do  {
+($InstanceState = (Get-EC2Instance -Region $Region -InstanceId $PVInstance.InstanceId).Instances.State.Name.Value)
+(SLEEP 5)    
+}
+Until ($InstanceState -eq "stopped")
+Write-Host "[$(get-date -Format "dd/mm/yy hh:mm:ss")] EC2 Instance is now stopped"
+
+# Move Data Volumes from PV to HVM Instance
+Write-Host ""
+Write-Host "[$(get-date -Format "dd/mm/yy hh:mm:ss")] Moving Data Volumes from PV Instance to new HVM Instance"
+
+foreach ($Volume in $PVVolumes[1..($PVVolumes.length)]) {
+    #Dismount Volume from PV Instance
+    Dismount-EC2Volume -Region $Region -InstanceId $PVInstance.InstanceId -VolumeId $Volume.VolumeId
+
+    #Wait until volume is dismounted
+    Do {
+        ($VolumeState = (Get-EC2Volume -Region $Region -VolumeId $Volume.VolumeId).State)
+        (SLEEP 2)
+    }
+    Until ($VolumeState -eq "available")
+
+    #Add Volume HVM Instance
+    Add-EC2Volume -Region $Region -InstanceId $HVMInstance.InstanceId -VolumeId $Volume.VolumeId -Device $Volume.attachments.device
+}
+
+Write-Host "[$(get-date -Format "dd/mm/yy hh:mm:ss")] Data Volumes migrated from PV Instance to new HVM Instance successfully"
