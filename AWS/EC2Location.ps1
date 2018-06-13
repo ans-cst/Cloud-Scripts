d8412cf7ab5e2d72ecac91b04e4de0d92dd1f9e7
ans-cst <Cloudservices.Servicedesk@ansgroup.co.uk>
2018-06-13 15:19:07 +0100

    Cleaning up scripts that are no longer required

--- AWS/EC2Location.ps1
+++
@@ -1,4 +0,0 @@
-foreach ($vm in (Get-EC2Instance -Region eu-west-1 -Filter @{Name="availability-zone";Value="eu-west-1*"}).Instances)
-{
- $tag = $vm.tags | ? { $_.key -eq "Name"} | Select -ExpandProperty Value;
- Write-host ($tag+"|"+$vm.Instanceid+"|"+$vm.Placement.AvailabilityZone) | Format-Table | Out-File -FilePath C:\Users\rfroggatt\desktop\AZBalance.csv -Encoding ASCII -Width 512};
