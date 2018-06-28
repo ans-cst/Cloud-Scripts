10bb19c0939e2f61b9be84af354acc49e59f7041
ans-cst <Cloudservices.Servicedesk@ansgroup.co.uk>
2018-06-28 11:29:25 +0100

    Templates and Deployment Script for alternative Service Outage Monitoring

---
+++ Azure/Service Monitoring/Service Outage Deploy.ps1
@@ -0,0 +1,45 @@
+﻿Write-Host ""
+Write-Host "ANS - Azure Service Outage Notification Deployment"
+Write-Host "Version 3.0.0"
+Write-Host ""
+Write-Host ""
+
+#Install and Import AzureRM Module
+Write-Host "[$(get-date -Format "dd/mm/yy hh:mm:ss")] Importing module..."
+Import-Module -Name AzureRM -ErrorVariable ModuleError -ErrorAction SilentlyContinue
+If ($ModuleError) {
+    Write-Host "[$(get-date -Format "dd/mm/yy hh:mm:ss")] Installing module..."
+    Install-Module -Name AzureRM
+    Import-Module -Name AzureRM
+    Write-Host "[$(get-date -Format "dd/mm/yy hh:mm:ss")] Successfully Installed module..."
+}
+Write-Host "[$(get-date -Format "dd/mm/yy hh:mm:ss")] Successfully Imported module"
+Write-Host ""
+
+#Login to Azure
+Write-Host "[$(get-date -Format "dd/mm/yy hh:mm:ss")] Logging in to Azure Account..."
+Login-AzureRmAccount
+Write-Host "[$(get-date -Format "dd/mm/yy hh:mm:ss")] Successfully logged in to Azure Account"
+Write-Host ""
+
+#Select SubscriptionId
+$SubId = Read-Host "Please input your Subscription Id"
+while ($SubId.Length -le 35) {
+    Write-Host "[$(get-date -Format "dd/mm/yy hh:mm:ss")] Subscription Id not valid"
+    $SubId = Read-Host "Please input your Subscription Id"
+}
+Select-AzureRmSubscription -SubscriptionId $SubId
+Write-Host "[$(get-date -Format "dd/mm/yy hh:mm:ss")] Subscription successfully selected"
+Write-Host ""
+
+
+$RGName = Read-Host "Please enter the Resource Group Name to create"
+$Location = Read-Host "Please enter the location to host the Resource Group"
+$Path = Read-Host "Enter directory path to Deployment Templates"
+
+# Create Resource Group
+New-AzureRmResourceGroup -Name $RGName -Location $Location
+
+# Deploy Service Outage Template
+New-AzureRmResourceGroupDeployment -ResourceGroupName $RGName -TemplateFile $Path+'\AzureServiceHealth.json' `
+-TemplateParameterFile $Path+'\AzureServiceHealth.parameters.json'
